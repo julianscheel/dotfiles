5ea7d6f7afe4286bf169f9bb6c962ff337ecfd43
Julian Scheel <julian@jusst.de>
2020-03-25 08:54:53 +0100

    fish: Various convenience tweaks
    Signed-off-by: Julian Scheel <julian@jusst.de>

--- fish/config.fish
+++ fish/config.fish
@@ -4,8 +4,19 @@ set fish_key_bindings fish_vi_key_bindings
 envoy -pf | source
 set -x VISUAL vim
 
+set -x GPG_TTY (tty)
+
 alias parents="pstree -h -s %self"
 alias bell='echo \a'
+alias vim=nvim
+
+function cdc --wraps "mkdir" --description 'Create directory and change into it'
+    mkdir $argv[1]; and cd $argv[1]
+end
+
+function wlscreenshot --wraps "grim" --description 'Capture screnshot of wayland window'
+    grim -g (swaymsg -t get_tree | jq -r '.. | select(.pid? and .visible?) | .rect | "\(.x),\(.y) \(.width)x\(.height)"' | slurp) $argv[1]
+end
 
 # Git convenience
 alias g="git"
@@ -13,10 +24,17 @@ alias gcp="git cherry-pick"
 alias gbs="git bisect"
 alias grb="git rebase"
 alias gco="git checkout"
+alias gvm="vim +/HEAD (git diff --name-only | uniq)"
+alias gam="git add (git diff --name-only | uniq)"
 
 alias gbd="git branch -d"
-function gbdr --description 'Delete git remote branch'
+function gbdr --wraps "git branch -d" --description 'Delete git remote branch'
     git push origin :$argv[1]
+end
+
+function gbd! --wraps "git branch -d" --description 'Delete git local and remote branch'
+    gbd $argv[1]
+    gbdr $argv[1]
 end
 
 # Systemd
