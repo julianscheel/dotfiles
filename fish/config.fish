4a4aae966bfb213eb94ba45c42fe532adfd2df1a
Julian Scheel <julian@jusst.de>
2022-09-12 14:24:36 +0200

    Update fish config
    Signed-off-by: Julian Scheel <julian@jusst.de>

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,9 @@
 set fish_key_bindings fish_vi_key_bindings
 
-#envoy -l; or envoy -a /home/julian/.ssh/id_dsa
-envoy -pf | source
 set -x VISUAL vim
 
 set -x GPG_TTY (tty)
+set -x XZ_DEFAULTS "-T 0"
 
 alias parents="pstree -h -s %self"
 alias bell='echo \a'
@@ -41,6 +40,46 @@ end
 # Systemd
 alias sc="systemctl"
 
+# Project navigation
+alias p="cd /nas/projects"
+alias b="cd /nas/business\ administration/projects"
+alias p2="p; cd 2002-stream-unlimited"
+alias b2="b; cd 2002-stream-unlimited"
+function p2x --description 'goto stream-unlimited project (fuzzy match)'
+    p2
+    cd (ls -1 . | grep $argv[1] | head -n1)
+end
+
+function px --description 'goto project (fuzzy match)'
+    p
+    cd (ls -1 . | grep $argv[1] | head -n1)
+end
+
+function bx --description 'goto business project (fuzzy match)'
+    b
+    cd (ls -1 . | grep $argv[1] | head -n1)
+end
+
+function b2x --description 'goto business stream-unlimited project (fuzzy match)'
+    b2
+    cd (ls -1 . | grep $argv[1] | head -n1)
+end
+
+alias kapa="open /nas/projects/9000-jusst-internal/02-project-management/resource-planning/(date +%Y)/(date +%yW%V)-resource-planning.pdf"
+
+# Docker/StreamSDK development
+alias streamsdk-build-legacy="/home/julian/dev/su/streamsdk-dev-tools/docker/run-legacy -s /home/julian/dev/su/"
+
+# Bitwarden
+alias bwunlock "bw unlock | grep export | cut -c 3- | source"
+function bwcp --description 'copy bitwarden password'
+    if test "$XDG_SESSION_TYPE" = "x11"
+        bw get password $argv[1] | xclip -selection clipboard
+    else
+        bw get password $argv[1] | wl-copy
+    end
+end
+
 set -l envfile "$HOME/.gnome-keyring.env"
 gnome-keyring-daemon --start --components=gpg,ssh,secrets > $envfile
 set var_set (cat "$envfile" |  sed -e 's/^\(.*\)/set -x \\1/' -e 's/=/ /' -e 's/\(.*\)$/\1;/')
@@ -59,8 +98,42 @@ switch $TERMINAL
 		end
 end
 
+# Dart
+set -gx PATH $PATH $HOME/.pub-cache/bin
+
+# Rust
+set -gx PATH $PATH $HOME/.cargo/bin
+
+# doc-templates
+set -gx PATH $PATH $HOME/git/doc-templates
+
+# lease viewer
+set -gx LEASES_URL http://unifi:8082/leases
+
+alias nsdk-run "/home/julian/dev/su/streamsdk-dev-tools/docker/run -s /home/julian/dev/su/"
+alias nsdk-run-19.04 "/home/julian/dev/su/streamsdk-dev-tools/docker/run-19.04 -s /home/julian/dev/su/"
+alias nsdk-run-20.02 "/home/julian/dev/su/streamsdk-dev-tools/docker/run-20.02 -s /home/julian/dev/su/"
+alias nsdk-run-20.03 "/home/julian/dev/su/streamsdk-dev-tools/docker/run-20.03 -s /home/julian/dev/su/"
+alias nsdk-run-21.01 "/home/julian/dev/su/streamsdk-dev-tools/docker/run-21.01 -s /home/julian/dev/su/"
+alias nsdk-run-21.02 "/home/julian/dev/su/streamsdk-dev-tools/docker/run-21.02 -s /home/julian/dev/su/"
+alias nsdk-run-21.03 "/home/julian/dev/su/streamsdk-dev-tools/docker/run-21.03 -s /home/julian/dev/su/"
+alias nsdk-run-22.01 "/home/julian/dev/su/streamsdk-dev-tools/docker/run-22.01 -s /home/julian/dev/su/"
+alias nsdk-run-22.02 "/home/julian/dev/su/streamsdk-dev-tools/docker/run-22.02 -s /home/julian/dev/su/"
+alias nsdk-run-legacy "/home/julian/dev/su/streamsdk-dev-tools/docker/run-legacy -s /home/julian/dev/su/"
+
+# fzf
+#fzf_key_bindings
+set -x FZF_DEFAULT_COMMAND "fd --type f"
+#set -x FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
+#set -x FZF_DEFAULT_OPTS "--preview 'bat --style=numbers --color=always {} | head -500'"
+
 # Base16 Shell
 if status --is-interactive
     set BASE16_SHELL "$HOME/.config/base16-shell/"
     source "$BASE16_SHELL/profile_helper.fish"
 end
+
+# Env setup aliases
+alias _xmos "pushd /opt/XMOS/XTC/15.1.4/; bass source SetEnv; popd"
+alias _flutter "source ~/env/flutter.fish"
+alias _flutter-1.2 "source ~/env/flutter-1.2.fish"
