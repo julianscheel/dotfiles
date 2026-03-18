224836fa8319d1744a4c6d4ae01d5ea915524608
Julian Scheel <julian@jusst.de>
2026-03-18 17:50:30 +0100

    fish: Various updates
    Use tinty for theming, setup fvm, add a few convenience command aliases.

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,5 @@
 set fish_key_bindings fish_vi_key_bindings
-
-set -x VISUAL vim
+fish_helix_key_bindings
 
 set -x GPG_TTY (tty)
 set -x XZ_DEFAULTS "-T 0"
@@ -9,17 +8,33 @@ alias parents="pstree -h -s %self"
 alias bell='echo \a'
 alias vim=nvim
 
-function cdc --wraps "mkdir" --description 'Create directory and change into it'
+# Sync helix and hx aliases based on what is installed
+if type -q helix; and not type -q hx
+    alias hx='helix'
+else if type -q hx; and not type -q helix
+    alias helix='hx'
+end
+
+alias ls='eza -b --hyperlink'
+alias tree='eza -b --hyperlink --tree'
+
+# Silence fish greeting
+set fish_greeting
+
+function cdc --wraps mkdir --description 'Create directory and change into it'
     mkdir $argv[1]; and cd $argv[1]
 end
 
-function wlscreenshot --wraps "grim" --description 'Capture screnshot of wayland window'
+function wlscreenshot --wraps grim --description 'Capture screnshot of wayland window'
     grim -g (swaymsg -t get_tree | jq -r '.. | select(.pid? and .visible?) | .rect | "\(.x),\(.y) \(.width)x\(.height)"' | slurp) $argv[1]
+end
+
+function deccanlog --description 'View a deccan log export in systemd json format'
+    fx $argv[1] 'x => `${new Date(x.__REALTIME_TIMESTAMP / 1e3)} ${x._COMM} [${x._PID}]: ${x.MESSAGE}`'
 end
 
 # Git convenience
 alias g="git"
-alias gcp="git cherry-pick"
 alias gbs="git bisect"
 
 alias grb="git rebase"
@@ -30,7 +45,11 @@ alias gr="git commit"
 alias ga="git commit --amend"
 alias gas="git commit --amend -s"
 
-alias gco="git checkout"
+alias gdc="git diff --cached"
+
+alias gsw="git switch"
+alias grs="git restore"
+
 alias gvm="vim +/HEAD (git diff --name-only | uniq)"
 alias gam="git add (git diff --name-only | uniq)"
 alias grh="git reset --hard @{upstream}"
@@ -76,37 +95,30 @@ end
 alias kapa="open /nas/projects/9000-jusst-internal/02-project-management/resource-planning/(date +%Y)/(date +%yW%V)-resource-planning.pdf"
 
 # Docker/StreamSDK development
-alias streamsdk-build-legacy="/home/julian/dev/su/streamsdk-dev-tools/docker/run-legacy -s /home/julian/dev/su/"
+alias streamsdk-build-legacy="$HOME/dev/su/streamsdk-dev-tools/docker/run-legacy -s $HOME/dev/su/"
 
 # Bitwarden
-alias bwunlock "bw unlock | grep export | cut -c 3- | source"
 function bwcp --description 'copy bitwarden password'
-    if test "$XDG_SESSION_TYPE" = "x11"
-        bw get password $argv[1] | xclip -selection clipboard
-    else
-        bw get password $argv[1] | wl-copy
-    end
-end
+    argparse 'f/field=' -- $argv
 
-switch (uname)
-case Linux
-	set -l envfile "$HOME/.gnome-keyring.env"
-	gnome-keyring-daemon --start --components=gpg,ssh,secrets > $envfile
-	set var_set (cat "$envfile" |  sed -e 's/^\(.*\)/set -x \\1/' -e 's/=/ /' -e 's/\(.*\)$/\1;/')
-	eval $var_set
+    if set -ql _flag_field
+        rbw get -f $_flag_field $argv[1] --clipboard
+    else
+        rbw get $argv[1] --clipboard
+    end
 end
 
 # Fix del key in st
 switch $TERMINAL
-	case 'st'
-		#tput smkx
-		function fish_enable_keypad_transmit --on-event fish_postexec
-			tput smkx
-		end
+    case st
+        #tput smkx
+        function fish_enable_keypad_transmit --on-event fish_postexec
+            tput smkx
+        end
 
-		function fish_disable_keypad_transmit --on-event fish_preexec
-			tput rmkx
-		end
+        function fish_disable_keypad_transmit --on-event fish_preexec
+            tput rmkx
+        end
 end
 
 # Dart
@@ -114,6 +126,7 @@ set -gx PATH $PATH $HOME/.pub-cache/bin
 
 # Rust
 set -gx PATH $PATH $HOME/.cargo/bin
+set -gx PATH $PATH /usr/lib/rustup/bin
 
 # doc-templates
 set -gx PATH $PATH $HOME/git/doc-templates
@@ -121,16 +134,17 @@ set -gx PATH $PATH $HOME/git/doc-templates
 # lease viewer
 set -gx LEASES_URL http://unifi:8082/leases
 
-alias nsdk-run "/home/julian/dev/su/streamsdk-dev-tools/docker/run -s /home/julian/dev/su/"
-alias nsdk-run-19.04 "/home/julian/dev/su/streamsdk-dev-tools/docker/run-19.04 -s /home/julian/dev/su/"
-alias nsdk-run-20.02 "/home/julian/dev/su/streamsdk-dev-tools/docker/run-20.02 -s /home/julian/dev/su/"
-alias nsdk-run-20.03 "/home/julian/dev/su/streamsdk-dev-tools/docker/run-20.03 -s /home/julian/dev/su/"
-alias nsdk-run-21.01 "/home/julian/dev/su/streamsdk-dev-tools/docker/run-21.01 -s /home/julian/dev/su/"
-alias nsdk-run-21.02 "/home/julian/dev/su/streamsdk-dev-tools/docker/run-21.02 -s /home/julian/dev/su/"
-alias nsdk-run-21.03 "/home/julian/dev/su/streamsdk-dev-tools/docker/run-21.03 -s /home/julian/dev/su/"
-alias nsdk-run-22.01 "/home/julian/dev/su/streamsdk-dev-tools/docker/run-22.01 -s /home/julian/dev/su/"
-alias nsdk-run-22.02 "/home/julian/dev/su/streamsdk-dev-tools/docker/run-22.02 -s /home/julian/dev/su/"
-alias nsdk-run-legacy "/home/julian/dev/su/streamsdk-dev-tools/docker/run-legacy -s /home/julian/dev/su/"
+alias nsdk-run "$HOME/dev/su/streamsdk-dev-tools/docker/run -s $HOME/dev/su/"
+alias nsdk-run-19.04 "$HOME/dev/su/streamsdk-dev-tools/docker/run-19.04 -s $HOME/dev/su/"
+alias nsdk-run-20.02 "$HOME/dev/su/streamsdk-dev-tools/docker/run-20.02 -s $HOME/dev/su/"
+alias nsdk-run-20.03 "$HOME/dev/su/streamsdk-dev-tools/docker/run-20.03 -s $HOME/dev/su/"
+alias nsdk-run-21.01 "$HOME/dev/su/streamsdk-dev-tools/docker/run-21.01 -s $HOME/dev/su/"
+alias nsdk-run-21.02 "$HOME/dev/su/streamsdk-dev-tools/docker/run-21.02 -s $HOME/dev/su/"
+alias nsdk-run-21.03 "$HOME/dev/su/streamsdk-dev-tools/docker/run-21.03 -s $HOME/dev/su/"
+alias nsdk-run-22.01 "$HOME/dev/su/streamsdk-dev-tools/docker/run-22.01 -s $HOME/dev/su/"
+alias nsdk-run-22.02 "$HOME/dev/su/streamsdk-dev-tools/docker/run-22.02 -s $HOME/dev/su/"
+alias nsdk-run-23.03 "$HOME/dev/su/streamsdk-dev-tools/docker/run-23.03 -s $HOME/dev/su/"
+alias nsdk-run-legacy "$HOME/dev/su/streamsdk-dev-tools/docker/run-legacy -s $HOME/dev/su/"
 
 # fzf
 #fzf_key_bindings
@@ -141,13 +155,22 @@ set -x FZF_DEFAULT_COMMAND "fd --type f"
 # zoxide
 zoxide init fish | source
 
-# Base16 Shell
-if status --is-interactive
-    set BASE16_SHELL "$HOME/.config/base16-shell/"
-    source "$BASE16_SHELL/profile_helper.fish"
-end
+# setup theme
+tinty init
 
 # Env setup aliases
-alias _xmos "pushd /opt/XMOS/XTC/15.2.1/; bass source SetEnv; popd"
-alias _flutter "source ~/env/flutter.fish"
-alias _flutter-1.2 "source ~/env/flutter-1.2.fish"
+alias _xmos "pushd /opt/XMOS/XTC/15.3.1/; bass source SetEnv; popd"
+
+# fvm flutter version managemant
+set -x PATH "$PATH:$HOME/fvm/default/bin"
+
+if type -q micromamba
+
+    # >>> mamba initialize >>>
+    # !! Contents within this block are managed by 'mamba init' !!
+    set -gx MAMBA_EXE /usr/bin/micromamba
+    set -gx MAMBA_ROOT_PREFIX $HOME/micromamba
+    $MAMBA_EXE shell hook --shell fish --root-prefix $MAMBA_ROOT_PREFIX | source
+    # <<< mamba initialize <<<
+
+end
